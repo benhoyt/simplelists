43db9c63780911bf3dd2e513efcec61ce0a8d878
Ben Hoyt <benhoyt@gmail.com>
2023-02-16 22:22:47 +1300

    Make config use environment variables instead of command line args

--- main.go
+++ main.go
@@ -16,13 +16,29 @@ import (
 )
 
 func main() {
-	defaultPort := 8080
-	dbPath := flag.String("db", "simplelists.sqlite", "`path` to SQLite 3 database")
-	timezone := flag.String("timezone", "", "IANA timezone `name` (default local)")
-	port := flag.Int("port", 0, fmt.Sprintf("HTTP `port` to listen on (default $PORT or %d)", defaultPort))
-	showLists := flag.Bool("lists", false, "show lists on homepage")
-	username := flag.String("username", "", "optional username to access site")
-	genPass := flag.Bool("genpass", false, "interactively generate password hash (don't run server)")
+	// Config defaults
+	port := 8080
+	dbPath := "simplelists.sqlite"
+	showLists := false
+	timezone := ""
+	username := ""
+
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), `Usage: simplelists [options]
+
+Options:
+  -genpass              create password hash (instead of running server)
+
+Environment variables:
+  PORT                  HTTP port to listen on (default %d)
+  SIMPLELISTS_DB        path to SQLite 3 database (default %q)
+  SIMPLELISTS_LISTS     show lists on homepage (if set to 1 or "true")
+  SIMPLELISTS_PASSHASH  password hash (required if username is set)
+  SIMPLELISTS_TIMEZONE  IANA timezone name (defaults to local timezone)
+  SIMPLELISTS_USERNAME  optional username to access site
+`, port, dbPath)
+	}
+	genPass := flag.Bool("genpass", false, "-")
 	flag.Parse()
 
 	if *genPass {
@@ -40,8 +56,29 @@ func main() {
 		return
 	}
 
+	// Parse config from environment variables
+	var err error
+	if portEnv, ok := os.LookupEnv("PORT"); ok {
+		port, err = strconv.Atoi(portEnv)
+		if err != nil {
+			exitOnError(err)
+		}
+	}
+	if dbEnv, ok := os.LookupEnv("SIMPLELISTS_DB"); ok {
+		dbPath = dbEnv
+	}
+	if listsEnv, ok := os.LookupEnv("SIMPLELISTS_LISTS"); ok {
+		showLists = listsEnv == "1" || listsEnv == "true"
+	}
+	if timezoneEnv, ok := os.LookupEnv("SIMPLELISTS_TIMEZONE"); ok {
+		timezone = timezoneEnv
+	}
+	if usernameEnv, ok := os.LookupEnv("SIMPLELISTS_USERNAME"); ok {
+		username = usernameEnv
+	}
+
 	var passwordHash string
-	if *username != "" {
+	if username != "" {
 		passwordHash = os.Getenv("SIMPLELISTS_PASSHASH")
 		if passwordHash == "" {
 			log.Fatal("SIMPLELISTS_PASSHASH must be set if username is set")
@@ -50,23 +87,17 @@ func main() {
 		exitOnError(err)
 	}
 
-	db, err := sql.Open("sqlite", *dbPath)
+	db, err := sql.Open("sqlite", dbPath)
 	exitOnError(err)
 	model, err := NewSQLModel(db)
 	exitOnError(err)
-	server, err := NewServer(model, log.Default(), *timezone, *username, passwordHash, *showLists)
+	server, err := NewServer(model, log.Default(), timezone, username, passwordHash, showLists)
 	exitOnError(err)
 
-	if *port == 0 {
-		port = &defaultPort
-		envPort, err := strconv.Atoi(os.Getenv("PORT"))
-		if err == nil {
-			port = &envPort
-		}
-	}
-
-	log.Printf("listening on http://localhost:%d", *port)
-	err = http.ListenAndServe(":"+strconv.Itoa(*port), server)
+	log.Printf("config: port=%d db=%q lists=%v timezone=%q username=%q",
+		port, dbPath, showLists, timezone, username)
+	log.Printf("listening on http://localhost:%d", port)
+	err = http.ListenAndServe(":"+strconv.Itoa(port), server)
 	exitOnError(err)
 }
 
